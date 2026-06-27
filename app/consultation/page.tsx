d1ff637d385a96d0e496272b8fd3f06f18e29172
Siraajadams <siraaj.adams2003@gmail.com>
2026-06-27 12:15:26 +0200

    Update page.tsx

--- app/consultation/page.tsx
+++ app/consultation/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useMemo, useRef, useState } from "react";
 import Link from "next/link";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabase";
 
 type Patient = {
   id: string;
