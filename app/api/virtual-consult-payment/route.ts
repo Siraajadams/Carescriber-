be960b79ff8bd6679b054c97a97111d20ca51121
Siraajadams <siraaj.adams2003@gmail.com>
2026-08-01 14:39:10 +0200

    Create route.ts

---
+++ app/api/virtual-consult-payment/route.ts
@@ -0,0 +1,9 @@
+metadata: {
+  referralCode,
+  consentToken,
+  consultationReason,
+  patientId: patientId || "",
+  patientName: patientName || "",
+  patientFirstName: patientFirstName || "",
+  patientSurname: patientSurname || "",
+},
