032c1e999929a244f8255ba9cc66e4e997452248
Siraajadams <siraaj.adams2003@gmail.com>
2026-08-04 02:10:12 +0200

    Update ICD10Search.tsx

--- app/components/ICD10Search.tsx
+++ app/components/ICD10Search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useRef, useState } from "react";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 type ICD10Code = {
   id: number;
